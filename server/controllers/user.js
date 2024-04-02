7c0abed4f31f8140baa9688689168112fecdfa98
Prafull Singh <prafullsingh975@gmail.com>
2024-04-02 18:01:44 +0530

    hash password and incomplete login controller

---
+++ server/controllers/user.js
@@ -0,0 +1,66 @@
+const bcrypt = require("bcrypt");
+const User = require("../models/user.js");
+
+// upload profile left
+const registerUser = async (req, res) => {
+  try {
+    // destructure req.body
+    const { firstName, lastName, email, password, gender, age } = req.body;
+    // data validation required
+    if (!firstName.trim() || !email.trim() || !password.trim())
+      return res.status(400).json("Firstname,email,password is required");
+    // finding user on the basis of email
+    const isUser = await User.findOne({ email });
+    if (isUser) return res.status(400).json("Email is already used");
+    // Creating username
+    const dataArr = email.split("@");
+    const userName = dataArr[0];
+    // Hashing plain text password
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    // user registration
+    const newUser = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hash,
+      gender,
+      age,
+      userName,
+    });
+
+    const user = await User.findById(newUser._id).select("-password");
+
+    return res
+      .status(201)
+      .json({ message: "User registered successfully", data: user });
+  } catch (error) {
+    console.log("Error while user registration", error);
+    return res
+      .status(500)
+      .json(`Error while user registration ${error.message}`);
+  }
+};
+const logInUser = async (req, res) => {
+  try {
+    // destructure
+    const { email, password } = req.body;
+    // Validate
+    if (!email.trim() || !password.trim())
+      return res.status(400).json("Email password requires");
+    // fetch user
+    const isUser = await User.findOne({ email: email });
+    if (!isUser) return res.status(404).json("User not found");
+    // check password
+    const isMatch = await bcrypt.compare(password,isUser.password);
+    if(!isMatch) return res.status(403).json("Invalid password")
+    // generate token
+const token = "jwt"
+    // send to frontend
+  } catch (error) {
+    console.log("Error in User login ", error);
+    res.status(500).json(error.message);
+  }
+};
+
+module.exports = { registerUser, logInUser };
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,42 +1,9 @@
 const express = require("express");
-const User = require("../models/user.js");
+const { registerUser, logInUser } = require("../controllers/user.js");
 const router = express.Router();
 
-router.post("/register", async (req, res) => {
-  try {
-    // destructure req.body
-    const { firstName, lastName, email, password, gender, age } = req.body;
-    // data validation required
-    if (!firstName.trim() || !email.trim() || !password.trim())
-      return res.status(400).json("Firstname,email,password is required");
-    // finding user on the basis of email
-    const isUser = await User.findOne({ email });
-    if (isUser) return res.status(400).json("Email is already used");
-    // Creating username
-    const dataArr = email.split("@");
-    const userName = dataArr[0];
-    // Hashing plain text password
-    const hash = "";
-    // user registration
-    const newUser = await User.create({
-      firstName,
-      lastName,
-      email,
-      password,
-      gender,
-      age,
-      userName,
-    });
+router.post("/register",registerUser );
+router.post("/login",logInUser)
 
-    return res
-      .status(201)
-      .json({ message: "User registered successfully", data: newUser });
-  } catch (error) {
-    console.log("Error while user registration", error);
-    return res
-      .status(500)
-      .json(`Error while user registration ${error.message}`);
-  }
-});
 
 module.exports = router;
