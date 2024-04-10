fa776e285d137554d35628a82bc214580ab1d100
Prafull Singh <prafullsingh975@gmail.com>
2024-04-10 17:50:47 +0530

    add image to post

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,3 +1,4 @@
+const { cloudinaryUplaod } = require("../middleware/cloudinary.js");
 const Post = require("../models/post.js");
 const User = require("../models/user");
 
@@ -7,15 +8,23 @@ const createPost = async (req, res) => {
     const { title, content } = req.body;
     // validation
     if (!title?.trim()) return res.status(400).json("Title is required");
+    let response;
+    if (req?.file?.path) {
+      response = await cloudinaryUplaod(req.file.path);
+    }
     // craete post
     const newPost = await Post.create({
       title,
       content,
+      image: response? response.url : "",
       createdBy: req.user._id,
     });
-    const post = await Post.findById(newPost._id).populate({path:"createdBy",select:"firstName lastName"});
+    const post = await Post.findById(newPost._id).populate({
+      path: "createdBy",
+      select: "firstName lastName",
+    });
     const user = await User.findById(req.user._id);
-    const posts = [...user.posts,newPost._id]; // [1,2,3,4,6]
+    const posts = [...user.posts, newPost._id]; // [1,2,3,4,6]
     user.posts = posts;
     await user.save();
 
@@ -27,4 +36,19 @@ const createPost = async (req, res) => {
   }
 };
 
-module.exports = { createPost };
+const likePost = async(req,res)=>{
+  try {
+    const {postId} = req.body;
+    if(!postId)return res.status(400).json("Post id is required");
+
+    const post = await Post.findById(postId);
+    if(!post)return res.status(404).json("Post not found");
+
+    
+  } catch (error) {
+    console.log("Error in post like",error);
+    return res.status(500).json(error.message)
+  }
+}
+
+module.exports = { createPost, likePost };
--- server/routes/post.js
+++ server/routes/post.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { isLoggedIn } = require("../middleware/auth");
 const { createPost } = require("../controllers/post");
+const upload = require("../middleware/multer");
 const router  = express.Router();
 
-router.post('/create',isLoggedIn,createPost)
+router.post('/create',isLoggedIn,upload.single('img'),createPost);
+router.patch('/like',isLoggedIn,)
 
 module.exports = router
