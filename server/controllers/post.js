43015cf87e10971322814e8965dd9714e08f416a
Prafull Singh <prafullsingh975@gmail.com>
2024-04-12 17:44:17 +0530

    post liked and got all user feeds

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -16,7 +16,7 @@ const createPost = async (req, res) => {
     const newPost = await Post.create({
       title,
       content,
-      image: response? response.url : "",
+      image: response ? response.url : "",
       createdBy: req.user._id,
     });
     const post = await Post.findById(newPost._id).populate({
@@ -36,19 +36,52 @@ const createPost = async (req, res) => {
   }
 };
 
-const likePost = async(req,res)=>{
+const likePost = async (req, res) => {
   try {
-    const {postId} = req.body;
-    if(!postId)return res.status(400).json("Post id is required");
+    const { postId } = req.body;
+    if (!postId) return res.status(400).json("Post id is required");
 
     const post = await Post.findById(postId);
-    if(!post)return res.status(404).json("Post not found");
+    if (!post) return res.status(404).json("Post not found");
+    const likes = post.likes;
+    const indexAt = likes?.findIndex(
+      (elm) => String(elm) == String(req.user._id)
+    );
+    let message;
+    if (indexAt >= 0) {
+      likes.splice(indexAt, 1);
+      message = "Dislike";
+    } else {
+      likes.push(req.user._id);
+      message = "like";
+    }
 
-    
+    post.likes = likes;
+    await post.save();
+    return res.status(200).json(message);
   } catch (error) {
-    console.log("Error in post like",error);
-    return res.status(500).json(error.message)
+    console.log("Error in post like", error);
+    return res.status(500).json(error.message);
   }
-}
+};
 
-module.exports = { createPost, likePost };
+const showAllPost = async (req, res) => {
+  try {
+    const allPosts = await Post.find({ createdBy: { $nin: req.user._id } });
+    return res.status(200).json(allPosts);
+  } catch (error) {
+    console.log("Error in getting all posts", error);
+    return res.status(500).json(error.message);
+  }
+};
+const showUserAllPost = async (req, res) => {
+  try {
+    const allUserPosts = await Post.find({ createdBy: req.user._id });
+    return res.status(200).json(allUserPosts);
+  } catch (error) {
+    console.log("Error in getting all posts", error);
+    return res.status(500).json(error.message);
+  }
+};
+
+module.exports = { createPost, likePost, showAllPost, showUserAllPost };
--- server/routes/post.js
+++ server/routes/post.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { isLoggedIn } = require("../middleware/auth");
-const { createPost } = require("../controllers/post");
+const { createPost, likePost, showAllPost, showUserAllPost } = require("../controllers/post");
 const upload = require("../middleware/multer");
 const router  = express.Router();
 
 router.post('/create',isLoggedIn,upload.single('img'),createPost);
-router.patch('/like',isLoggedIn,)
+router.patch('/like',isLoggedIn,likePost);
+router.get('/all',isLoggedIn,showAllPost)
+router.get('/my-feed',isLoggedIn,showUserAllPost)
 
 module.exports = router
